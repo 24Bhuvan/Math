3639209a863741337c9c4a82fd352332fb8082b7
24Bhuvan <Ubhuvan533@gmail.com>
2025-02-02 11:41:35 +0530

    Percentile-Gives percentage of a given data that is under the given value

--- Percentile.py
+++ Percentile.py
@@ -1,8 +1,8 @@
 import numpy
 
-def percentile(arr,percentage_below):
-    return numpy.percentile(arr,percentage_below)
-    
+def percentile(arr,percentage):
+    return numpy.percentile(arr,percentage)
+
 if __name__=="__main__":
     Pb= int(input("Enter the percentage of the percentile to be calculated: "))
     n=int(input("Enter size of array: "))
@@ -10,4 +10,4 @@ if __name__=="__main__":
     for i in range(n):
         item=int(input(f"Enter your {i} element: "))
         ls.append(item)
-    print(percentile(ls,Pb))
+    print(percentile(ls,Pb))   
